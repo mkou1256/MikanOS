e72fc5a126f66acdb5dc22587b81af7fcc1586cf
mkou1256 <zenokoki3@gmail.com>
2025-07-13 21:58:32 +0900

    format

--- src/hello.c
+++ src/hello.c
@@ -4,25 +4,22 @@ typedef void *EFI_HANDLE;
 
 struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 typedef EFI_STATUS (*EFI_TXEXT_STRING)(
-    struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
-    CHAR16                                  *String);
+    struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
 
 typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    void             *dummy;
-    EFI_TXEXT_STRING OutputString;
+  void *dummy;
+  EFI_TXEXT_STRING OutputString;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-typedef struct 
-{
-    char                            dummy[52];
-    EFI_HANDLE                      ConsoleOutHandle;
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+typedef struct {
+  char dummy[52];
+  EFI_HANDLE ConsoleOutHandle;
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
 } EFI_SYSTEM_TABLE;
 
-EFI_STATUS EfiMain(EFI_HANDLE       ImageHandle,
-                   EFI_SYSTEM_TABLE *SystemTable)
-{
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, world!\n");
-    while(1);
-    return 0;
+EFI_STATUS EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, world!\n");
+  while (1)
+    ;
+  return 0;
 }
